a69a448ce62eb6819e3075948ec0e3ee900617a7
Bartek <bart.matusz@gmail.com>
2020-11-07 22:41:53 +0100

    entity mappings

---
+++ Application-data/src/main/kotlin/com/gruzini/data/mappings.kt
@@ -0,0 +1,39 @@
+package com.gruzini.data
+
+import org.jetbrains.exposed.sql.ResultRow
+
+fun ResultRow.toArena() : Arena = Arena(
+        id = this[Arenas.id],
+        name = this[Arenas.name],
+        seats = this[Arenas.seats]
+)
+
+fun ResultRow.toTeam() : Team = Team(
+        id = this[Teams.id],
+        name = this[Teams.name],
+        abbreviation = this[Teams.abbreviation],
+        state = this[Teams.state],
+        city = this[Teams.city],
+        arenaId = this[Teams.arenaId],
+        coachId = this[Teams.coachId]
+)
+
+fun ResultRow.toCoach() : Coach = Coach(
+        id = this[Coaches.id],
+        firstName = this[Coaches.firstName],
+        surname = this[Coaches.surname],
+        age = this[Coaches.age],
+        exp = this[Coaches.exp],
+        teamId = this[Coaches.teamId]
+)
+
+fun ResultRow.toPlayer() : Player = Player(
+        id = this[Players.id],
+        firstName = this[Players.firstName],
+        surname = this[Players.surname],
+        height = this[Players.height],
+        weight = this[Players.weight],
+        age = this[Players.age],
+        exp = this[Players.exp],
+        teamId = this[Players.teamId]
+)
